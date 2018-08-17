c9c2d8749ae1f817a80033c689ec8d394ecfb374
Vinoth.N <vinoth.n@alphind.com>
2018-08-17 15:01:26 +0530

    webback revert

--- ImageToTest/webpack.config.js
+++ ImageToTest/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-//const AotPlugin = require('@ngtools/webpack').AotPlugin;
-const AngularCompilerPlugin = require('@ngtools/webpack').AngularCompilerPlugin;
+const AotPlugin = require('@ngtools/webpack').AotPlugin;
+//const AngularCompilerPlugin = require('@ngtools/webpack').AngularCompilerPlugin;
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
 
 module.exports = (env) => {
@@ -46,8 +46,8 @@ module.exports = (env) => {
         ] : [
             // Plugins that apply in production builds only
             new webpack.optimize.UglifyJsPlugin(),
-            //new AotPlugin({
-                new AngularCompilerPlugin({
+            new AotPlugin({
+            //    new AngularCompilerPlugin({
                 tsConfigPath: './tsconfig.json',
                 entryModule: path.join(__dirname, 'ClientApp/app/app.browser.module#AppModule'),
                 exclude: ['./**/*.server.ts']
@@ -68,8 +68,8 @@ module.exports = (env) => {
             })
         ].concat(isDevBuild ? [] : [
             // Plugins that apply in production builds only
-            //new AotPlugin({
-            new AngularCompilerPlugin({
+            new AotPlugin({
+            //new AngularCompilerPlugin({
                 tsConfigPath: './tsconfig.json',
                 entryModule: path.join(__dirname, 'ClientApp/app/app.server.module#AppModule'),
                 exclude: ['./**/*.browser.ts']
